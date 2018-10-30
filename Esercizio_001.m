cc20e9e801ca52b89221018414ba4e95277735e4
Daniele Olivieri <danieledaniele1988@gmail.com>
2018-10-30 22:25:09 +0100

    Primi file

---
+++ Esercizio_001.m
@@ -0,0 +1,63 @@
+R1=1;
+R2=2;
+E3=3;
+J4=4;
+n=3;elle=4;
+
+A=[...
+-1 0 1 0
+1 1 0 -1];
+
+B=[...
+    1 0 1 1
+    0 1 0 1];
+
+M=[...
+    -R1 0 0 0
+    0 -R2 0 0
+    0 0 0 0
+    0 0 0 1];
+
+N=[...
+    1 0 0 0
+    0 1 0 0
+    0 0 1 0
+    0 0 0 0];
+
+b=[...
+    0
+    0
+    E3
+    J4];
+
+%tableau
+T = [
+    A zeros(n-1,elle)
+    zeros(elle-n+1,elle) B
+    M N ];
+    
+c=[...
+    zeros(n-1,1)
+    zeros(elle-(n-1),1)
+    b];
+   
+x=T\c;
+
+%separa nel vettore delle incognite correnti e tensioni
+
+i=x(1:elle);
+v=x(elle+1:end);
+
+% potenze
+P=v.*i;
+Ptot1=v.'*i;
+Ptot2=sum(P);
+
+% stampe e postprocessing
+fprintf('Potenze assorbite dai bipoli:\n');
+for kk=1:elle
+    fprintf('P%d = %d\n',kk,P(kk))
+end
+fprintf('potenza totale');
+fprintf(Ptot2);
+
---
+++ Esercizio_003_Thevenin.m
@@ -0,0 +1,47 @@
+R1=1; R2 =2; R3 = 3; R4 =4; R5 =5; E6 = 6; J7 = 7; E8 = 8;
+
+n=5;elle=8;
+
+
+%matrice A
+A = -[...
+    1   0   0   0   0   -1  0   0
+    -1  0   -1  0   1   0   1   0  
+    0   0  0   -1  -1   0   -1  1
+    0   -1  0   0   0   1   0   0];
+
+%matrice M
+M = diag([-R1 -R2 -R3 -R4 -R5 0 1 0]);
+
+%matrice N
+N = diag([1 1 1 1 1 1 0 1]);
+
+%vettore termini noti b
+b = [...
+    zeros(n-1,1)
+    zeros(5,1)
+    E6
+    J7
+    E8];
+
+%Matrice tableau
+T = [ A zeros(n-1,n-1)
+    M N*A'];
+
+x=T\b
+
+i=x(1:elle)
+v=x(elle+1:end)
+
+% potenziale tra A e C
+PE6 = v(3)-v(1);
+fprintf('Tensione equivalente di Thevenin tra i morsetti A e C = %d \n',PE6);
+
+
+%grafico
+figure
+bar(v)
+xlabel('Indice di lato')
+ylabel('Potenziali nodali');
+grid on
+
